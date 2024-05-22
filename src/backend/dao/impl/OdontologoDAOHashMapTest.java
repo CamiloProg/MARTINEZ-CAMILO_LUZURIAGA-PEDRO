d85e357c612c27b498d40c45843b61d9d3d891ea
CamiloProg <camilom200107@gmail.com>
2024-05-21 21:17:56 -0500

    last

--- src/backend/dao/impl/OdontologoDAOHashMapTest.java
+++ src/backend/dao/impl/OdontologoDAOHashMapTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class OdontologoDAOHashMapTest {
     private OdontologoDAO odontologoDAO;
@@ -15,6 +16,26 @@ public class OdontologoDAOHashMapTest {
     @Before
     public void setUp() {
         odontologoDAO = new OdontologoDAOHashMap();
+    }
+
+
+    @Test
+    public void testGuardar() {
+        Odontologo odontologo1 = new Odontologo(1, "1234", "camilo", "Juan");
+        Odontologo odontologo2 = new Odontologo(2, "5678", "Gomez", "gustavo");
+
+        odontologoDAO.guardar(odontologo1);
+        odontologoDAO.guardar(odontologo2);
+
+        List<Odontologo> odontologos = odontologoDAO.listarTodos();
+
+        // Verifica que se han guardado dos odontólogos
+        assertEquals(2, odontologos.size());
+
+        // Verifica que los IDs se han asignado correctamente
+        assertNotNull(odontologo1.getId());
+        assertNotNull(odontologo2.getId());
+
     }
 
     @Test
